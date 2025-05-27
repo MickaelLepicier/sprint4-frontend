7722c96d871da0b7845496b22c4fc72b1d67e533
Boaz Teller <boaz.teller@gmail.com>
2025-05-27 20:35:19 +0300

    refactor: organize imports and clean up RootCmp structure

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
 import { Routes, Route, Navigate } from 'react-router'
 
+// Services
 import { userService } from './services/user'
+
+// Pages
 import { HomePage } from './pages/HomePage'
 import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
-import { CarIndex } from './pages/CarIndex.jsx'
-import { ReviewIndex } from './pages/ReviewIndex.jsx'
-import { ChatApp } from './pages/Chat.jsx'
-import { AdminIndex } from './pages/AdminIndex.jsx'
-
+import { CarIndex } from './pages/CarIndex'
 import { CarDetails } from './pages/CarDetails'
+import { ReviewIndex } from './pages/ReviewIndex'
+import { ChatApp } from './pages/Chat'
+import { AdminIndex } from './pages/AdminIndex'
 import { UserDetails } from './pages/UserDetails'
+import { LoginSignup } from './pages/LoginSignup'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
 
+// Components
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
-import { UserMsg } from './cmps/UserMsg.jsx'
-import { LoginSignup } from './pages/LoginSignup.jsx'
-import { Login } from './pages/Login.jsx'
-import { Signup } from './pages/Signup.jsx'
-
+import { UserMsg } from './cmps/UserMsg'
 
 export function RootCmp() {
     return (
@@ -29,40 +30,44 @@ export function RootCmp() {
             <main>
                 <Routes>
                     <Route path="" element={<HomePage />} />
+
                     <Route path="about" element={<AboutUs />}>
                         <Route path="team" element={<AboutTeam />} />
                         <Route path="vision" element={<AboutVision />} />
                     </Route>
+
                     <Route path="car" element={<CarIndex />} />
                     <Route path="car/:carId" element={<CarDetails />} />
                     <Route path="user/:id" element={<UserDetails />} />
                     <Route path="review" element={<ReviewIndex />} />
                     <Route path="chat" element={<ChatApp />} />
+
                     <Route path="admin" element={
                         <AuthGuard checkAdmin={true}>
                             <AdminIndex />
                         </AuthGuard>
                     } />
+
                     <Route path="login" element={<LoginSignup />}>
                         <Route index element={<Login />} />
                         <Route path="signup" element={<Signup />} />
                     </Route>
                 </Routes>
             </main>
+
             <AppFooter />
         </div>
     )
 }
 
-
-
-
 function AuthGuard({ children, checkAdmin = false }) {
     const user = userService.getLoggedinUser()
     const isNotAllowed = !user || (checkAdmin && !user.isAdmin)
+
     if (isNotAllowed) {
         console.log('Not Authenticated!')
         return <Navigate to="/" />
     }
+
     return children
 }

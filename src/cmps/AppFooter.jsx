6a4c4def18303249518b8011419ca88c96f3076c
WhiteBeltFullStack <zangetsu923@gmail.com>
2025-05-29 14:18:22 +0300

    chore: apply sending song to footer via youtube

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,14 +1,27 @@
 import { useSelector } from 'react-redux'
 
 export function AppFooter() {
-    const count = useSelector(storeState => storeState.userModule.count)
+  const count = useSelector(storeState => storeState.userModule.count)
+  const song = useSelector(storeState => storeState.stationModule.currentSong) || ''
 
-    return (
-        <footer className="app-footer main-container full">
-            <section>
-                <p>Coffeerights &copy; 2024</p>
-                <p>Count: {count}</p>
-            </section>
-        </footer>
-    )
+  function getEmbedUrl(url) {
+    if (!url) return
+    const match = url.match(/v=([a-zA-Z0-9_-]+)/)
+    return match ? `https://www.youtube.com/embed/${match[1]}` : ''
+  }
+
+  return (
+    <footer className="app-footer main-container full">
+      <section className="song-player-section">
+        <iframe
+          width="420"
+          height="50"
+          src={getEmbedUrl(song.url)}
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+        ></iframe>
+      </section>
+    </footer>
+  )
 }

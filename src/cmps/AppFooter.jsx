2e57fa5f98efaaeb3df7b7b64012a9d8d2746531
WhiteBeltFullStack <zangetsu923@gmail.com>
2025-05-30 02:35:29 +0300

    keep both changes

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,14 +1,31 @@
 import { MediaPlayer } from './MediaPlayer'
 
+import { useSelector } from 'react-redux'
+
 export function AppFooter() {
+  const count = useSelector(storeState => storeState.userModule.count)
+  const song = useSelector(storeState => storeState.stationModule.currentSong) || ''
+
+  function getEmbedUrl(url) {
+    if (!url) return
+    const match = url.match(/v=([a-zA-Z0-9_-]+)/)
+    return match ? `https://www.youtube.com/embed/${match[1]}` : ''
+  }
 
   return (
     <footer className="app-footer flex full">
       {/* <MediaPlayer /> */}
+
+      <section className="song-player-section">
+        <iframe
+          width="420"
+          height="50"
+          src={getEmbedUrl(song.url)}
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+        ></iframe>
+      </section>
     </footer>
   )
 }
-
-
-
-

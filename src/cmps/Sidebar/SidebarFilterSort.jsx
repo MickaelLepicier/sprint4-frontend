9f950b1b0dec1c7b697d0aff9387e34888ee0277
BoazTeller <boaz.teller@gmail.com>
2025-06-09 23:37:20 +0300

    feat: add underlay line and a label when dragging playlists

--- src/cmps/Sidebar/SidebarFilterSort.jsx
+++ src/cmps/Sidebar/SidebarFilterSort.jsx
@@ -1,17 +1,40 @@
+import { useRef, useState } from "react"
+import { SearchIcon } from "../svg/SearchIcon"
+
 export function SidebarFilterSort({ searchTerm, setSearchTerm }) {
+    const [active, setActive] = useState(false)
+    const searchInputRef = useRef(null)
+
+    function openSearch() {
+        setActive(true)
+        setTimeout(() => searchInputRef.current?.focus(), 10)
+    }
+
+    function closeSearch() {
+        if (!searchTerm) setActive(false)
+    }
+
     return (
         <section className="sidebar-filter-sort">
             <input
+                ref={searchInputRef}
                 type="text"
-                className="library-search-input"
+                className={`library-search-input${active ? " active" : ""}`}
                 placeholder="Search in Your Library"
                 value={searchTerm}
                 onChange={ev => setSearchTerm(ev.target.value)}
+                onBlur={closeSearch}
                 aria-label="Type to filter your library"
             />
-            {/* <button class="search-icon-btn" aria-label="Search in Your Library">
-                🔍
-            </button>    */}
+
+            <button
+                className="search-icon-btn flex align-center justify-center"
+                aria-label="Search in Your Library"
+                type="button"
+                onClick={openSearch}
+            >
+                <SearchIcon />
+            </button>   
         </section>
     )
 }

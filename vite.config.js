b7e1042032c9b34f07005be912c236139ec7c349
BoazTeller <boaz.teller@gmail.com>
2025-06-10 23:06:35 +0300

    chore: add fonts and update typography accordingly

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
-// Vite configuration for React + SWC with custom output dir
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
     build: {
         outDir: '../backend/public',
         emptyOutDir: true
     }
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// // Vite configuration for React + SWC with custom output dir
+// export default defineConfig({
+//     plugins: [react()],
+//     build: {
+//         outDir: '../backend/public',
+//         emptyOutDir: true
+//     }
+// })

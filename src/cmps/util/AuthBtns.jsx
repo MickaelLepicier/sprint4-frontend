cf1f1d17a3d12c04650c7de7ce2f80ab6af456a8
WhiteBeltFullStack <zangetsu923@gmail.com>
2025-06-14 20:24:43 +0300

    chore: logout by clicking on user

--- src/cmps/util/AuthBtns.jsx
+++ src/cmps/util/AuthBtns.jsx
@@ -1,6 +1,7 @@
 import { NavLink, useNavigate } from 'react-router'
 import { logout } from '../../store/user/user.actions'
 import { showErrorMsg, showSuccessMsg } from '../../services/event-bus.service'
+import { useState, useRef, useEffect } from 'react'
 
 export function AuthBtns({ user, isHeader = true }) {
   return isHeader ? <HeaderAuthButs user={user} /> : <FooterAuthLink />
@@ -8,6 +9,32 @@ export function AuthBtns({ user, isHeader = true }) {
 
 function HeaderAuthButs({ user }) {
   const navigate = useNavigate()
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
+  const menuRef = useRef(null)
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      console.log('event:', menuRef.current.contains(event.target))
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpenModal(false)
+      }
+    }
+
+    if (isOpenModal) {
+      document.addEventListener('mousedown', handleClickOutside)
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [isOpenModal])
+
+  function showUserModal(isOpen) {
+    setIsOpenModal(isOpen)
+  }
 
   async function onLogout() {
     try {
@@ -22,9 +49,31 @@ function HeaderAuthButs({ user }) {
   return (
     <section className="auth-header">
       {user ? (
-        <button className="btn-logout" onClick={onLogout}>
-          <span className="btn-span">Logout</span>
-        </button>
+        <div className="flex">
+          {/* <button className="btn-logout" onClick={onLogout}>
+            <span className="btn-span">Logout</span>
+          </button> */}
+          <div className="menu-modal" ref={menuRef}>
+            <button
+              className="user-icon"
+              onClick={() => {
+                showUserModal(!isOpenModal)
+              }}
+            >
+              <span className="user-name" title={user.fullname}>
+                {user.fullname.substring(0, 1)}
+              </span>
+            </button>{' '}
+            {/*maybe another div around btn*/}
+            {isOpenModal && (
+              <ul className="user-menu">
+                <li className="user-logout" onClick={onLogout}>
+                  Log out
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
       ) : (
         <>
           <NavLink to="signup" className="btn-signup">

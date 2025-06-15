10afa7fc0c9d18940fcfd39c09c8daf3013c8075
WhiteBeltFullStack <zangetsu923@gmail.com>
2025-06-15 05:29:36 +0300

    f-edit-album

--- src/cmps/Sidebar/Sidebar.jsx
+++ src/cmps/Sidebar/Sidebar.jsx
@@ -11,103 +11,102 @@ import { SidebarList } from './SidebarList'
 import { showSuccessMsg, showErrorMsg } from '../../services/event-bus.service'
 
 export function Sidebar({ isCollapsed, setIsCollapsed }) {
-    const [searchTerm, setSearchTerm] = useState('')
-    const [isSidebarHovered, setIsSidebarHovered] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isSidebarHovered, setIsSidebarHovered] = useState(false)
 
-    const stations = useSelector(state => state.stationModule.stations)
-    const user = useSelector(state => state.userModule.user)
-    const navigate = useNavigate()
+  const stations = useSelector(state => state.stationModule.stations)
+  const user = useSelector(state => state.userModule.user)
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        if (!stations.length) loadStations()
-    }, [stations.length])
+  console.log('Sidebar stations:', stations)
 
-    
+  useEffect(() => {
+    if (!stations.length) loadStations()
+  }, [stations.length])
 
-    // Reserverd for future filter when refactoring the entire component
-    function filterStations(stationsArr) {
-        if (!searchTerm.trim()) return stationsArr
-        const regex = new RegExp(searchTerm, 'i')
-        return stationsArr.filter(station => regex.test(station.name))
-    }
-
+  // Reserverd for future filter when refactoring the entire component
+  function filterStations(stationsArr) {
+    if (!searchTerm.trim()) return stationsArr
     const regex = new RegExp(searchTerm, 'i')
+    return stationsArr.filter(station => regex.test(station.name))
+  }
 
-    // Stations added  by user to his library
-    // const likedStations = user ? stations.filter(s => user.likedStationIds.includes(s._id)) : []
-    const likedStations = user ? stations.filter(s =>user.likedStationIds.includes(s._id) && regex.test(s.name)) : []
+  const regex = new RegExp(searchTerm, 'i')
 
-    // Stations the user created (*** DOES NOT INCLUDE THE LIKED SONGS STATION! ***)
-    // const userStations = user
-    //     ? stations.filter(s => s.createdBy?._id === user._id && s._id !== user.likedSongsStationId)
-    //     : []
-    const userStations = user ? stations.filter(s => s.createdBy?._id === user._id && s._id !== user.likedSongsStationId && regex.test(s.name)) : []
+  // Stations added  by user to his library
+  // const likedStations = user ? stations.filter(s => user.likedStationIds.includes(s._id)) : []
+  const likedStations = user ? stations.filter(s => user.likedStationIds.includes(s._id) && regex.test(s.name)) : []
 
-    // Stations that contains all the songs the user has liked
-    // Station shows/hides according if there are any songs in it
-    // const likedSongsStation = stations.find(s => s._id === user?.likedSongsStationId)
-    const likedSongsStation = stations.find(s => s._id === user?.likedSongsStationId && regex.test(s.name))
-    const likedSongsCount = likedSongsStation?.songs?.length || 0
+  // Stations the user created (*** DOES NOT INCLUDE THE LIKED SONGS STATION! ***)
+  // const userStations = user
+  //     ? stations.filter(s => s.createdBy?._id === user._id && s._id !== user.likedSongsStationId)
+  //     : []
+  const userStations = user
+    ? stations.filter(s => s.createdBy?._id === user._id && s._id !== user.likedSongsStationId && regex.test(s.name))
+    : []
 
-    async function onCreateStation() {
-        if (!user) {
-        showErrorMsg('Please log in to create a songlist')
-        return
-        }
+  // Stations that contains all the songs the user has liked
+  // Station shows/hides according if there are any songs in it
+  // const likedSongsStation = stations.find(s => s._id === user?.likedSongsStationId)
+  const likedSongsStation = stations.find(s => s._id === user?.likedSongsStationId && regex.test(s.name))
+  const likedSongsCount = likedSongsStation?.songs?.length || 0
 
-        try {
-            const savedStation = await createStationForUser(user)
-            showSuccessMsg('New songlist created!')
-
-            await loadStations()
-            
-            navigate(`/playlist/${savedStation._id}`)
-        } catch (err) {
-                showErrorMsg('Failed to create songlist')
-            }
-        }
-
-    function onToggleCollapse() {
-        setIsCollapsed(prevIsCollapsed => !prevIsCollapsed)
+  async function onCreateStation() {
+    if (!user) {
+      showErrorMsg('Please log in to create a songlist')
+      return
     }
 
-    return (
-        <aside 
-            className={`sidebar${isCollapsed ? ' collapsed' : ''}`}
-            onMouseEnter={() => setIsSidebarHovered(true)}
-            onMouseLeave={() => setIsSidebarHovered(false)}
-        >
-            <SidebarHeader 
-                onCreateStation={onCreateStation}
-                isCollapsed={isCollapsed}
-                onToggleCollapse={onToggleCollapse}  
-                isSidebarHovered={isSidebarHovered}  
-            />
+    try {
+      const savedStation = await createStationForUser(user)
+      showSuccessMsg('New songlist created!')
 
-            {user && (
-                <>
-                    {/* Only show playlist/arists filter when expanded */}
-                    { !isCollapsed && <SidebarViewFilter /> }
-                    
-                    <div className="library-container">
-                        {/* Only show search bar & sort when expanded */}
-                        { !isCollapsed && <SidebarFilterSort 
-                                                searchTerm={searchTerm} 
-                                                setSearchTerm={setSearchTerm} 
-                                          /> }
-                        
-                        <SidebarList
-                            userStations={userStations}
-                            likedStations={likedStations}
-                            likedSongsStation={likedSongsStation}
-                            likedSongsCount={likedSongsCount}
-                            likedSongsStationId={user?.likedSongsStationId}
-                            user={user}
-                            isCollapsed={isCollapsed}
-                        />
-                    </div>
-                </>
-            )}
-        </aside>
-    )
+      await loadStations()
+
+      navigate(`/playlist/${savedStation._id}`)
+    } catch (err) {
+      showErrorMsg('Failed to create songlist')
+    }
+  }
+
+  function onToggleCollapse() {
+    setIsCollapsed(prevIsCollapsed => !prevIsCollapsed)
+  }
+
+  return (
+    <aside
+      className={`sidebar${isCollapsed ? ' collapsed' : ''}`}
+      onMouseEnter={() => setIsSidebarHovered(true)}
+      onMouseLeave={() => setIsSidebarHovered(false)}
+    >
+      <SidebarHeader
+        onCreateStation={onCreateStation}
+        isCollapsed={isCollapsed}
+        onToggleCollapse={onToggleCollapse}
+        isSidebarHovered={isSidebarHovered}
+      />
+
+      {user && (
+        <>
+          {/* Only show playlist/arists filter when expanded */}
+          {!isCollapsed && <SidebarViewFilter />}
+
+          <div className="library-container">
+            {/* Only show search bar & sort when expanded */}
+            {!isCollapsed && <SidebarFilterSort searchTerm={searchTerm} setSearchTerm={setSearchTerm} />}
+
+            <SidebarList
+              userStations={userStations}
+              likedStations={likedStations}
+              likedSongsStation={likedSongsStation}
+              likedSongsCount={likedSongsCount}
+              likedSongsStationId={user?.likedSongsStationId}
+              user={user}
+              isCollapsed={isCollapsed}
+            />
+          </div>
+        </>
+      )}
+    </aside>
+  )
 }

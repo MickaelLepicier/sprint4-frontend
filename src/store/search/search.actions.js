cb2e6f53c68ab0d052c8f33d1beb3f50c01c3b55
BoazTeller <boaz.teller@gmail.com>
2025-06-27 18:20:12 +0300

    chore: add numResults prop to loadSearchResults

--- src/store/search/search.actions.js
+++ src/store/search/search.actions.js
@@ -14,7 +14,34 @@ function getCmdSetSearchResults(results) {
   return { type: SET_SEARCH_RESULTS, results }
 }
 
-export async function loadSearchResults(txt, searchType = 'songs') {
+// export async function loadSearchResults(txt, searchType = 'songs', numResults = 5) {
+//   try {
+//     store.dispatch(getCmdSetSearchText(txt))
+//     store.dispatch(getCmdSetSearchType(searchType))
+
+//     const searchFnMap = {
+//       songs: stationService.sideBarSearch,
+//       stations: stationService.headerSearch,
+//       genre: stationService.genreSonglistSearch
+//     }
+
+//     const searchFn = searchFnMap[searchType]
+//     if (!searchFn) {
+//       console.warn('Unknown search type:', searchType)
+//       // store.dispatch(getCmdSetSearchResults([]))
+//       // return []
+//     }
+
+//     const results = await searchFn(txt, numResults)
+//     store.dispatch(getCmdSetSearchResults(results))
+//     return results
+//   } catch (err) {
+//     console.log('Cannot load search results:', err)
+//     throw err
+//   }
+// }
+
+export async function loadSearchResults(txt, searchType = 'songs', numResults = 5) {
   try {
     store.dispatch(getCmdSetSearchText(txt))
     store.dispatch(getCmdSetSearchType(searchType))
@@ -22,11 +49,11 @@ export async function loadSearchResults(txt, searchType = 'songs') {
     let results
 
     if (searchType === 'songs') {
-      results = await stationService.sideBarSearch(txt)
+      results = await stationService.sideBarSearch(txt, numResults)
     } else if (searchType === 'stations') {
-      results = await stationService.headerSearch(txt)
+      results = await stationService.headerSearch(txt, numResults)
     } else if (searchType === 'genre') {
-      results = await stationService.genreSonglistSearch(txt)
+      results = await stationService.genreSonglistSearch(tx, numResults)
     } else {
       console.warn('Unknown search type:', searchType)
       results = []

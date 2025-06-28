20f4abe1449585691919a861e8e37e4cc0f79731
BoazTeller <boaz.teller@gmail.com>
2025-06-28 19:11:01 +0300

    fix: correct LikeIcon svg

--- src/cmps/svg/LikeIcon.jsx
+++ src/cmps/svg/LikeIcon.jsx
@@ -5,16 +5,16 @@ export function LikeIcon({ size = 16, ...props }) {
       data-encore-id="icon"
       role="img"
       aria-hidden="true"
-      viewBox="0 0 32 32"
+      viewBox="0 0 16 16"
       width={size}
       height={size}
-      fill="none"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <circle cx="16" cy="16" r="15" stroke="currentColor" strokeWidth="2" />
-      <line x1="16" y1="10" x2="16" y2="22" stroke="currentColor" strokeWidth="2" />
-      <line x1="10" y1="16" x2="22" y2="16" stroke="currentColor" strokeWidth="2" />
+      <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8"></path>
+      <path d="M11.75 8a.75.75 0 0 1-.75.75H8.75V11a.75.75 0 0 1-1.5 0V8.75H5a.75.75 0 0 1 0-1.5h2.25V5a.75.75 0 0 1 1.5 0v2.25H11a.75.75 0 0 1 .75.75"></path>
     </svg>
   )
 }
+

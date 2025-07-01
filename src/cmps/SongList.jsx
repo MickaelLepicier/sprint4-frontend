3a11cc11865379a52ffc18a379a4e77066c6e747
BoazTeller <boaz.teller@gmail.com>
2025-07-01 03:27:31 +0300

    refactor: PlaylistDetails and SongPreview improved styling with sticky header

--- src/cmps/SongList.jsx
+++ src/cmps/SongList.jsx
@@ -7,7 +7,6 @@ export function SongList({ songs, station, onTogglePlay, handleDragEnd, isLikedS
   const sentinelRef = useRef(null)
   const [isSticky, setIsSticky] = useState(false)
   const isStickyCN = isSticky ? 'is-sticky' : ''
-  // CN = ClassName
 
   const [selectedSongId, setSelectedSongId] = useState(null)
   const [justLikedSongId, setJustLikedSongId] = useState(null)
@@ -35,31 +34,38 @@ export function SongList({ songs, station, onTogglePlay, handleDragEnd, isLikedS
         setJustLikedSongId(null)
       }
     }
+
     document.addEventListener('mousedown', handleClickOutside)
+    
     return () => document.removeEventListener('mousedown', handleClickOutside)
   }, [])
 
-
   /*
-      TODO:
-      maybe put here SongList comp and make this comp PlayListDetails
-      use the function to make ...
-    */
+    TODO:
+    maybe put here SongList comp and make this comp PlayListDetails
+    use the function to make ...
+  */
+
   return (
     <div className="song-list-container" ref={listRef}>
-      <div ref={sentinelRef} style={{ height: '1px' }}></div> {/* for sticky effect */}
-      <div className={`song-list-header ${isStickyCN}`}>
-        <div className="col col-idx">#</div>
-        <div className="col col-title">Title</div>
-        <div className="col col-artist">Artist</div>
-        <div className="col col-album">Album</div>
-        <div className="col col-date">Date Added</div>
-        {/* {isLikedSongs && <div className="col col-date">Date Added</div>} */}
+      <div ref={sentinelRef} style={{ height: '1px' }}></div>
+      {/* <div ref={sentinelRef} style={{ height: '1px' }}></div> for sticky effect */}
+      
+      <div className={`song-list-header-container ${isStickyCN}`}>
+        <div className="song-list-header">
+          <div className="col col-idx">#</div>
+          <div className="col col-title">Title</div>
+          <div className="col col-artist">Artist</div>
+          <div className="col col-album">Album</div>
+          <div className="col col-date">Date Added</div>
+          {/* {isLikedSongs && <div className="col col-date">Date Added</div>} */}
 
-        <div className="col col-actions">
-          <ClockIcon />
+          <div className="col col-actions">
+            <ClockIcon />
+          </div>
         </div>
       </div>
+
       <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="songs-droppable" direction="vertical">
           {(provided) => (

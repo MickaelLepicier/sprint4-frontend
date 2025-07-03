3bbccb01b51326b9b6a01714a8ad468ad59527e2
BoazTeller <boaz.teller@gmail.com>
2025-07-03 05:40:32 +0300

    chore: rig the headerSearch for preloaded data

--- src/services/station/station.service.remote.js
+++ src/services/station/station.service.remote.js
@@ -14,6 +14,9 @@ const headerCache = loadFromStorage(HEADER_SEARCH_KEY) || {}
 
 const GENRE_SEARCH_KEY = 'genreCacheDB'
 const genreCache = loadFromStorage(GENRE_SEARCH_KEY) || {}
+
+const RIGGED_MODE = true
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // const YT_API_KEY = import.meta.env.VITE_YT_API_KEY
 const YT_API_KEYS = (import.meta.env.VITE_YT_API_KEYS || '')
@@ -175,95 +178,104 @@ async function sideBarSearch(query) {
     })
 }
 
-async function headerSearch(query, numResults = 5) {
-    const searchWord = query.trim().toLowerCase()
+// async function headerSearch(query, numResults = 5) {
+//     const searchWord = query.trim().toLowerCase()
 
-    if (headerCache[searchWord]?.length) {
-        return Promise.resolve(headerCache[searchWord])
-    }
+//     // Rigged demo
+//     const isLinkinPark = RIGGED_MODE && /link\s*in?\s*park|linkpark|linkinpark/i.test(searchWord)
+//     if (isLinkinPark) {
+//         const riggedStations = createRiggedLinkinParkStation(query)
+//         headerCache[searchWord] = riggedStations
+//         saveToStorage(HEADER_SEARCH_KEY, headerCache)
+//         return Promise.resolve(riggedStations)
+//     }
+    
+//     if (headerCache[searchWord]?.length) {
+//         return Promise.resolve(headerCache[searchWord])
+//     }
 
-    return withApiKeyRetry(async (apiKey) => {
-        const searchResults = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-            params: {
-                part: 'snippet',
-                q: query,
-                type: 'video',
-                maxResults: numResults,
-                key: apiKey, // swapped here
-            },
-        })
+//     return withApiKeyRetry(async (apiKey) => {
+//         const searchResults = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+//             params: {
+//                 part: 'snippet',
+//                 q: query,
+//                 type: 'video',
+//                 maxResults: numResults,
+//                 key: apiKey, // swapped here
+//             },
+//         })
 
-        const baseVideos = searchResults.data.items
+//         const baseVideos = searchResults.data.items
 
-        const artistStations = await Promise.all(
-            baseVideos.map(async (video, idx) => {
-                const artist = video.snippet.channelTitle
+//         const artistStations = await Promise.all(
+//             baseVideos.map(async (video, idx) => {
+//                 const artist = video.snippet.channelTitle
 
-                const artistRes = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-                    params: {
-                        part: 'snippet',
-                        q: artist,
-                        type: 'video',
-                        maxResults: 10,
-                        key: apiKey, // swapped here
-                    },
-                })
+//                 const artistRes = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+//                     params: {
+//                         part: 'snippet',
+//                         q: artist,
+//                         type: 'video',
+//                         maxResults: 10,
+//                         key: apiKey, // swapped here
+//                     },
+//                 })
 
-                const songs = artistRes.data.items
-                    .filter(item => item.id?.videoId)
-                    .map((item, i) => ({
-                        id: item.id.videoId,
-                        videoId: item.id.videoId,
-                        title: item.snippet.title,
-                        artist: item.snippet.channelTitle,
-                        imgUrl: item.snippet.thumbnails?.medium?.url || '',
-                        addedBy: `u10${i}`,
-                        likedBy: [`u20${i}`],
-                        addedAt: Date.now() - (i + 1) * 1000000,
-                    }))
+//                 const songs = artistRes.data.items
+//                     .filter(item => item.id?.videoId)
+//                     .map((item, i) => ({
+//                         id: item.id.videoId,
+//                         videoId: item.id.videoId,
+//                         title: item.snippet.title,
+//                         artist: item.snippet.channelTitle,
+//                         imgUrl: item.snippet.thumbnails?.medium?.url || '',
+//                         addedBy: `u10${i}`,
+//                         likedBy: [`u20${i}`],
+//                         addedAt: Date.now() - (i + 1) * 1000000,
+//                     }))
 
-                const videoIds = songs.map(song => song.videoId).join(',')
-                if (videoIds) {
-                    const detailsRes = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
-                        params: {
-                            part: 'contentDetails',
-                            id: videoIds,
-                            key: apiKey, // swapped here
-                        },
-                    })
+//                 const videoIds = songs.map(song => song.videoId).join(',')
+//                 if (videoIds) {
+//                     const detailsRes = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+//                         params: {
+//                             part: 'contentDetails',
+//                             id: videoIds,
+//                             key: apiKey, // swapped here
+//                         },
+//                     })
 
-                    const durationsMap = {}
-                    detailsRes.data.items.forEach(item => {
-                        durationsMap[item.id] = isoDurationToSeconds(item.contentDetails.duration)
-                    })
+//                     const durationsMap = {}
+//                     detailsRes.data.items.forEach(item => {
+//                         durationsMap[item.id] = isoDurationToSeconds(item.contentDetails.duration)
+//                     })
 
-                    songs.forEach(song => {
-                        song.duration = durationsMap[song.videoId] || 0
-                    })
-                }
+//                     songs.forEach(song => {
+//                         song.duration = durationsMap[song.videoId] || 0
+//                     })
+//                 }
 
-                const station = _createEmptyStation()
-                station._id = video.id?.videoId || `fallback-${idx}`
-                station.name = artist
-                station.imgUrl = video.snippet.thumbnails?.medium?.url || ''
-                station.tags = [query]
-                station.createdBy.fullname = artist
-                station.songs = songs
-                station.msgs = [
-                    { id: `m${idx}1`, from: 'u201', txt: 'Great vibes!' },
-                    { id: `m${idx}2`, from: 'u202', txt: 'Love it!' },
-                ]
+//                 const station = _createEmptyStation()
+//                 station._id = video.id?.videoId || `fallback-${idx}`
+//                 station.name = artist
+//                 station.imgUrl = video.snippet.thumbnails?.medium?.url || ''
+//                 station.tags = [query]
+//                 station.createdBy.fullname = artist
+//                 station.songs = songs
+//                 station.msgs = [
+//                     { id: `m${idx}1`, from: 'u201', txt: 'Great vibes!' },
+//                     { id: `m${idx}2`, from: 'u202', txt: 'Love it!' },
+//                 ]
 
-                return station
-            })
-        )
+//                 return station
+//             })
+//         )
 
-        headerCache[searchWord] = artistStations
-        saveToStorage(HEADER_SEARCH_KEY, headerCache)
+//         headerCache[searchWord] = artistStations
+//         saveToStorage(HEADER_SEARCH_KEY, headerCache)
 
-        return artistStations
-    })
-}
+//         return artistStations
+//     })
+// }
 
 async function genreSonglistSearch(genre, numResults = 5) {
   const searchWord = genre.trim().toLowerCase()
@@ -448,3 +460,204 @@ function _createEmptyStation() {
 //     const savedMsg = await httpService.post(`station/${stationId}/msg`, { txt })
 //     return savedMsg
 // }
+
+async function headerSearch(query, numResults = 5) {
+    const searchWord = query.trim().toLowerCase()
+    const isLinkinPark = RIGGED_MODE && /link\s*in?\s*park|linkpark|linkinpark/i.test(searchWord)
+
+    console.log('IS LINKIN PARK ',  isLinkinPark)
+    console.log("SEARCH WORD!!!!!!", searchWord, searchWord.length)
+
+    if (headerCache[searchWord]?.length) {
+        return Promise.resolve(headerCache[searchWord])
+    }
+
+    // Fetch the normal results for *every* query
+    const artistStations = await withApiKeyRetry(async (apiKey) => {
+        const searchResults = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+                part: 'snippet',
+                q: query,
+                type: 'video',
+                maxResults: numResults,
+                key: apiKey,
+            },
+        })
+
+        const baseVideos = searchResults.data.items
+
+        return Promise.all(
+            baseVideos.map(async (video, idx) => {
+                const artist = video.snippet.channelTitle
+
+                const artistRes = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        part: 'snippet',
+                        q: artist,
+                        type: 'video',
+                        maxResults: 10,
+                        key: apiKey,
+                    },
+                })
+
+                const songs = artistRes.data.items
+                    .filter(item => item.id?.videoId)
+                    .map((item, i) => ({
+                        id: item.id.videoId,
+                        videoId: item.id.videoId,
+                        title: item.snippet.title,
+                        artist: item.snippet.channelTitle,
+                        imgUrl: item.snippet.thumbnails?.medium?.url || '',
+                        addedBy: `u10${i}`,
+                        likedBy: [`u20${i}`],
+                        addedAt: Date.now() - (i + 1) * 1000000,
+                    }))
+
+                const videoIds = songs.map(song => song.videoId).join(',')
+                if (videoIds) {
+                    const detailsRes = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+                        params: {
+                            part: 'contentDetails',
+                            id: videoIds,
+                            key: apiKey,
+                        },
+                    })
+
+                    const durationsMap = {}
+                    detailsRes.data.items.forEach(item => {
+                        durationsMap[item.id] = isoDurationToSeconds(item.contentDetails.duration)
+                    })
+
+                    songs.forEach(song => {
+                        song.duration = durationsMap[song.videoId] || 0
+                    })
+                }
+
+                const station = _createEmptyStation()
+                station._id = video.id?.videoId || `fallback-${idx}`
+                station.name = artist
+                station.imgUrl = video.snippet.thumbnails?.medium?.url || ''
+                station.tags = [query]
+                station.createdBy.fullname = artist
+                station.songs = songs
+                station.msgs = [
+                    { id: `m${idx}1`, from: 'u201', txt: 'Great vibes!' },
+                    { id: `m${idx}2`, from: 'u202', txt: 'Love it!' },
+                ]
+                return station
+            })
+        )
+    })
+
+    // Only rig for linkin park!
+    if (isLinkinPark && artistStations.length) {
+        artistStations[0] = createRiggedLinkinParkStation(query)[0]
+    }
+
+    headerCache[searchWord] = artistStations
+    saveToStorage(HEADER_SEARCH_KEY, headerCache)
+    return Promise.resolve(artistStations)
+}
+
+
+
+function createRiggedLinkinParkStation(query = "linkin park") {
+    const now = Date.now()
+    const songs = [
+        {
+            id: "eVTXPUF4Oz4",
+            title: "In the End",
+            artist: "Linkin Park",
+            album: "Hybrid Theory",
+            imgUrl: "https://i.ytimg.com/vi/eVTXPUF4Oz4/mqdefault.jpg",
+            duration: 216
+        },
+        {
+            id: "kXYiU_JCYtU",
+            title: "Numb",
+            artist: "Linkin Park",
+            album: "Meteora",
+            imgUrl: "https://i.ytimg.com/vi/kXYiU_JCYtU/mqdefault.jpg",
+            duration: 185
+        },
+        {
+            id: "Gd9OhYroLN0",
+            title: "Somewhere I Belong",
+            artist: "Linkin Park",
+            album: "Meteora",
+            imgUrl: "https://i.ytimg.com/vi/Gd9OhYroLN0/mqdefault.jpg",
+            duration: 217
+        },
+        {
+            id: "8sgycukafqQ",
+            title: "What I've Done",
+            artist: "Linkin Park",
+            album: "Minutes to Midnight",
+            imgUrl: "https://i.ytimg.com/vi/8sgycukafqQ/mqdefault.jpg",
+            duration: 207
+        },
+        {
+            id: "LYU-8IFcDPw",
+            title: "Faint",
+            artist: "Linkin Park",
+            album: "Meteora",
+            imgUrl: "https://i.ytimg.com/vi/LYU-8IFcDPw/mqdefault.jpg",
+            duration: 162
+        },
+        {
+            id: "4qlCC1GOwFw",
+            title: "Breaking the Habit",
+            artist: "Linkin Park",
+            album: "Meteora",
+            imgUrl: "https://i.ytimg.com/vi/4qlCC1GOwFw/mqdefault.jpg",
+            duration: 208
+        },
+        {
+            id: "vjVkXlxsO8Q",
+            title: "Papercut",
+            artist: "Linkin Park",
+            album: "Hybrid Theory",
+            imgUrl: "https://i.ytimg.com/vi/vjVkXlxsO8Q/mqdefault.jpg",
+            duration: 185
+        },
+        {
+            id: "Gd9OhYroLN0",
+            title: "Crawling",
+            artist: "Linkin Park",
+            album: "Hybrid Theory",
+            imgUrl: "https://i.ytimg.com/vi/Gd9OhYroLN0/mqdefault.jpg",
+            duration: 209
+        },
+        {
+            id: "ScNNfyq3d_w",
+            title: "Waiting for the End",
+            artist: "Linkin Park",
+            album: "A Thousand Suns",
+            imgUrl: "https://i.ytimg.com/vi/ScNNfyq3d_w/mqdefault.jpg",
+            duration: 233
+        },
+        {
+            id: "zsCD5XCu6CM",
+            title: "Bleed It Out",
+            artist: "Linkin Park",
+            album: "Minutes to Midnight",
+            imgUrl: "https://i.ytimg.com/vi/zsCD5XCu6CM/mqdefault.jpg",
+            duration: 167
+        }
+    ]
+
+    // You can add more songs as needed
+
+    const station = _createEmptyStation()
+    station._id = songs[0].id 
+    station.name = "Linkin Park"
+    station.imgUrl = "https://i.ytimg.com/vi/SRXH9AbT280/mqdefault.jpg" // Nice LP pic
+    station.tags = [query]
+    station.createdBy = { fullname: "Linkin Park" }
+    station.songs = songs
+    station.msgs = [
+        { id: "m1", from: "u201", txt: "Great vibes!" },
+        { id: "m2", from: "u202", txt: "Love it!" }
+    ]
+    return [station]
+}
